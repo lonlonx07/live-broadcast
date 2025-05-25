2440d7874faf1d98edad0c50ab88c14ddce8db5b
lonlonx07 <lonlonx07@gmail.com>
2025-05-26 00:39:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from website import create_app
-
+import socket
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=socket.gethostbyname(socket.gethostname()), port=5554, debug=True)
